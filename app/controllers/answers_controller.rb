007c5605a638c3e383099ecc46b656322b30e1dd
bohdanshvetc <dan6147@ukr.net>
2022-01-20 14:23:54 +0200

    7

---
+++ app/controllers/answers_controller.rb
@@ -0,0 +1,53 @@
+class AnswersController < ApplicationController
+
+  before_action :set_question!
+  # before_action :set_answer!, except: :create
+
+  def edit
+    @answer = @question.answers.find params[:id]
+  end
+
+  def update
+    @answer = @question.answers.find params[:id]
+    if @answer.update answer_params
+      flash[:success] = "Answer updated!"
+      redirect_to question_path(@question, anchor: "answer-#{@answer.id}")
+    else
+      render :edit
+    end
+  end
+
+  def create
+    @answer = @question.answers.build answer_params
+
+    if @answer.save
+      flash[:success] = "Answer create!"
+      redirect_to question_path(@question, anchor: "answer-#{@answer.id}")
+    else
+      @answers = @question.answers.order created_at: :desc
+      render 'questions/show'
+    end
+  end
+
+  def destroy
+    answer = @question.answers.find params[:id]
+    answer.destroy
+    flash[:success] = "Answer deleted!"
+    redirect_to questions_path
+  end
+
+  private
+
+  def answer_params
+    params.require(:answer).permit(:body)
+  end
+
+  def set_question!
+    @question = Question.find(params[:question_id])
+  end
+
+  # def set_answer!
+  #   @answer = @question.answers.find params[:id]
+  # end
+  
+end
---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,61 @@
+class QuestionsController < ApplicationController
+
+  # before_action :set_question!, only: %i[show destroy edit update]
+
+  def show
+    @question = Question.find params[:id]
+    @answer = @question.answers.build
+    @answers = @question.answers.order(created_at: :desc).page(params[:page]).per(5)
+    # @answers = Answer.where(question: @question).limit(2).order(created_at: :desc)
+  end
+
+  def destroy
+    @question = Question.find params[:id]
+    @question.destroy
+    flash[:success] = "Question deleted!"
+    redirect_to questions_path
+  end
+
+  def edit
+    @question = Question.find params[:id]
+  end
+
+  def update
+    @question = Question.find params[:id]
+     if @question.update question_params
+      flash[:success] = "Question updated!"
+      redirect_to question_path(@question)
+    else
+      render :edit
+    end
+  end
+
+  def index
+    @questions = Question.order(created_at: :desc).page params[:page]
+  end
+
+  def new
+    @question = Question.new
+  end
+
+  def create
+    @question = Question.new question_params
+    if @question.save
+      flash[:success] = "Question created!"
+      redirect_to questions_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def question_params
+    params.require(:question).permit(:title, :body)
+  end
+
+  # def set_question!
+  #   @question = Question.find params[:id]
+  # end
+
+end
